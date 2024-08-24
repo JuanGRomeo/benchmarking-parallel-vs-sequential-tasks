102b782bfd1c9ff213c36e6de608e5f61cb69e47
Juan Romeo <juan.romeo@azure.techlensglobal.com>
2024-08-24 13:31:02 +0200

    First working as expected approach

---
+++ Benchmarking.Paralell.Vs.Sequential/Benchmarking.Paralell.Vs.Sequential/Config.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
+
+namespace Benchmarking.Paralell.Vs.Sequential;
+
+public class Config : ManualConfig
+{
+    public Config()
+    {
+        AddExporter(MarkdownExporter.Default);
+        AddExporter(HtmlExporter.Default);
+
+        AddDiagnoser(new MemoryDiagnoser(new MemoryDiagnoserConfig(displayGenColumns: true)));
+        AddDiagnoser(ThreadingDiagnoser.Default);
+        AddDiagnoser(ExceptionDiagnoser.Default);
+
+        AddLogger(ConsoleLogger.Default);
+
+        AddColumnProvider(DefaultColumnProviders.Instance);
+        AddColumn(StatisticColumn.AllStatistics);
+    }
+}
+
---
+++ Benchmarking.Paralell.Vs.Sequential/Benchmarking.Paralell.Vs.Sequential/TaskBenchmark.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking.Paralell.Vs.Sequential;
+
+public class TaskBenchmark
+{
+    private List<int> data;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        data = Enumerable.Range(1, 100).ToList();
+    }
+
+    private async Task SimulateIOOperation(int delay)
+    {
+        await Task.Delay(delay);
+    }
+
+    [Benchmark]
+    public async Task ExecuteSequentially()
+    {
+        foreach (var item in data)
+        {
+            await SimulateIOOperation(10);
+        }
+    }
+
+    [Benchmark]
+    public async Task ExecuteInParallel()
+    {
+        var tasks = data.Select(item => SimulateIOOperation(10));
+        await Task.WhenAll(tasks);
+    }
+}
